365fe3072ead4291bd0c709f850598efabdd8b16
Simon <21604666>
2017-10-11 11:45:09 +0200

    edit fournisseur

---
+++ fournisseur/edit.php
@@ -0,0 +1,64 @@
+<!--PHP CODE-->
+
+<?php
+
+require_once '../bdd.php';
+//require_onde '..correction.php';
+$bdd = new Bdd();
+$bdd->connect();
+$bdd->getBdd()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$bdd->getBdd()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+if(isset($_POST['fournisseur']) && !empty($_POST['fournisseur'])) {
+
+$fournisseur = array(
+
+    'FO_NUMERO' => 'F07',
+    'FO_NOM' => isset ($_POST['fournisseur'])? $_POST['fournisseur']: '',
+
+);
+
+    //$bdd->update('CDI_FOURNISSEUR', $values);
+    $message = array('etat' => 'success', 'message' => 'vous avez bien rempli le formulaire');
+}
+elseif (isset ($_POST['test'])? $_POST['test']: FALSE == "true") {
+    $message = array('etat' => 'danger', 'message' => 'vous avez mal rempli le formulaire');
+}
+
+$fournisseur = $bdd->query('SELECT * FROM CDI_FOURNISSEUR WHERE FO_NUMERO=\'F07\'');
+
+var_dump($fournisseur);
+?>
+
+
+<!--INCLUDE HEADER-->
+<?php require_once '../header.php'?>
+
+
+<!--ALERT MESSAGE-->
+<?php
+if(isset($message['etat'])) {
+    echo '<div class="alert alert-'.$message['etat'].' alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+    '.$message['message'].'</div>';
+}
+?>
+
+<!--FORM-->
+<form action="" method="post">
+    <div class="form-group">
+        <label for="numéro">Numéro : <?php echo $fournisseur['FO_NUMERO'] ?> </label>
+    </div>
+    <div class="form-group">
+        <label for="fournisseur">Fournisseur : </label>
+        <input type="text" class="form-control" id="fournisseur" name="fournisseur" value="<?php echo $fournisseur['FO_NOM'] ?>">
+    </div>
+    <input type="hidden" value="true" name="test">
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+
+</form>
+
+<!--INLCUDE FOOTER-->
+<?php require_once '../footer.php'?>

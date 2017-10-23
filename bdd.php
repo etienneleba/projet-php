ce68c3659590fe068acc01aa6308a40985a2240c
etienne <etienne.leba@gmail.com>
2017-10-23 10:57:01 +0200

    getMaxId debug

--- bdd.php
+++ bdd.php
@@ -26,9 +26,9 @@ class Bdd {
         return $this->bdd->query($stat)->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function getMaxId($carac, $table) {
+    public function getMaxId($table,  $colonne) {
 
-        return $carac . $this->query("select CONVERT(max(CONVERT(substr(`CL_NUMERO`,2),UNSIGNED INTEGER))+1, CHAR(55)) as ID from `$table`")['ID'];
+        return $colonne[0] . $this->query("select CONVERT(max(CONVERT(substr(`".$colonne."_NUMERO`,2),UNSIGNED INTEGER))+1, CHAR(55)) as ID from `$table`")['ID'];
 
     }
 

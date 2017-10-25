ee10db7a767c272ebaa81ddcb4d457f79ecca7b3
Simon <21604666>
2017-10-25 14:52:23 +0200

    presque finit commande

---
+++ commande/delete.php
@@ -0,0 +1,21 @@
+<!--PHP CODE-->
+
+<?php
+
+require_once '../bdd.php';
+$bdd = new Bdd();
+$bdd->connect();
+$bdd->getBdd()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$bdd->getBdd()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+if(isset($_GET['id'])){
+$id=$_GET['id'];
+}
+
+
+$bdd->deleter('CDI_COMMANDE','CO',$id);
+
+header('Location: '.$_SERVER['HTTP_REFERER']);
+exit;
+
+?>
